bca1e210a9f4cd3fd57ce643080f4c990ab95182
Nam Tran <nam.tran.ext@personio.de>
2022-06-22 12:40:08 +0700

    stage2 - binding a port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,25 +7,24 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
-    //    ServerSocket serverSocket = null;
-    //    Socket clientSocket = null;
-    //    int port = 6379;
-    //    try {
-    //      serverSocket = new ServerSocket(port);
-    //      serverSocket.setReuseAddress(true);
-    //      // Wait for connection from client.
-    //      clientSocket = serverSocket.accept();
-    //    } catch (IOException e) {
-    //      System.out.println("IOException: " + e.getMessage());
-    //    } finally {
-    //      try {
-    //        if (clientSocket != null) {
-    //          clientSocket.close();
-    //        }
-    //      } catch (IOException e) {
-    //        System.out.println("IOException: " + e.getMessage());
-    //      }
-    //    }
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 6379;
+    try {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
+      clientSocket = serverSocket.accept();
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
   }
 }
