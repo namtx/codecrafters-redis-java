b30b59f98cbdf6a298ef4d7c60e86f265afc7769
Nam Tran <nam.tran.ext@personio.de>
2022-06-22 13:13:57 +0700

    stage3 - Respond to multiple PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,28 +6,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    try {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      while(true) {
-        clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String inputString = in.readLine();
-        System.out.println(inputString);
-        System.out.println("READLINE");
-        out.println("+PONG");
-      }
-    } catch (IOException e){
-      System.out.println("IOException: " + e.getMessage());
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while (true) {
+                String inputString = in.readLine();
+                System.out.println(inputString);
+                System.out.println("READLINE");
+                out.println("+PONG");
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
-  }
 }
