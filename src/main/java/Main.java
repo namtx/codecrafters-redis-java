a6d9648ffed3932265c1853c663f8e75bc5664e7
Nam Tran <nam.tran.ext@personio.de>
2022-06-22 12:55:44 +0700

    stage3 - Respond to PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,6 +18,12 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String greeting = in.readLine();
+      if ("PING".equals(greeting)) {
+        out.print("+PONG\r\n");
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
